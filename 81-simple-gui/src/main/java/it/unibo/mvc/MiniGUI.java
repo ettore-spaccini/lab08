dc9808b5ed6fbe758ad5bda50418d06aaa0ca12e
Ettore Spaccini <ettore.spaccini@studio.unibo.it>
2024-11-11 11:27:49 +0100

    Esercizio 81 completato e corretto dal prof

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,8 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -33,13 +36,22 @@ public class MiniGUI {
         canvas.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        /*
+        // new panel
+        final JPanel panel = new JPanel(); 
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        canvas.add(panel, BorderLayout.CENTER); 
+        panel.add(write, BorderLayout.CENTER); 
+        final JTextField textField = new JTextField("Result"); 
+        canvas.add(textField, BorderLayout.NORTH); 
+         /*
          * Handlers
          */
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                final int randomNum = randomGenerator.nextInt(); 
+                System.out.println(randomNum);
+                textField.setText("Result: " + randomNum); 
             }
         });
     }
